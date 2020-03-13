52eaecaf08e77da9315c4d09179e7449f8037a37
Daniel Mader <45594838+daniel-mader@users.noreply.github.com>
2020-03-14 00:08:20 +0100

    enhance styling of login form

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  showPassword = false;
+
   constructor() { }
 
   ngOnInit(): void {
+    console.log('init');
   }
 
 }
